54cf2eb12a763260fa4dcb39906963c705a0c624
saqqdy <saqqdy@qq.com>
2021-08-27 00:43:52 +0800

    改用ts

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,15 @@
  * @example let messager = new PostMessager(); messager.subscribe('action', (content) => { console.log(21, content); }); messager.postMessageUp('action', { up: 201 });
  */
 import { readonly } from 'core-decorators';
-import * as addEvent from 'js-cool/lib/addEvent'
-import * as removeEvent from 'js-cool/lib/removeEvent'
-import * as uuid from 'js-cool/lib/uuid'
+import { addEvent, removeEvent, uuid } from 'js-cool'
+// import * as removeEvent from 'js-cool/lib/removeEvent'
+// import * as uuid from 'js-cool/lib/uuid'
 
-interface Instance {
-    [type: string]: any
-}
-
-interface Content {
+export interface Content {
     actionName?: string
 }
 
-interface PostMessageEvent {
+export interface PostMessageEvent {
     data: {
         type?: string
         content: {
@@ -24,29 +20,40 @@ interface PostMessageEvent {
     }
 }
 
-interface PostMessager {
-    messager: object
-    uuid: string
-    instance: Instance
-    type: string
+interface PostMessagerType {
+    // messager: {
+    //     [prop: string]: any
+    // }
+    // uuid: string
+    instance: {
+        [type: string]: any
+    }
+    // type: string
 }
 
-class PostMessager implements Instance {
-    messager: object = {}
+
+class PostMessager implements PostMessagerType {
+    messager: {
+        [type: string]: any
+    } = {}
     @readonly
     uuid: string = uuid()
+    instance: {
+        [type: string]: any
+    }
+    type
     constructor(instance: object = {}, type: string = 'invokeCustomEvent') {
         this.instance = instance
         this.type = type
         // 创建message监听
-        if (typeof window === 'undefined') {
+        if (typeof (window as any) === 'undefined') {
             console.error('仅支持在浏览器端运行')
             return
         }
-        addEvent(window, 'message', this.createEventHandler.bind(this), false)
+        addEvent((window as any), 'message', this.createEventHandler.bind(this), false)
     }
     // 订阅消息
-    subscribe(actionName, handler) {
+    subscribe(actionName: string, handler: any): void {
         if ('actionName' in this.messager) console.warn('订阅方法名重复，已覆盖旧的订阅')
         this.messager[actionName] = handler
     }
@@ -55,7 +62,7 @@ class PostMessager implements Instance {
         delete this.messager[action]
     }
     // 创建message监听
-    createEventHandler({ data }: PostMessageEvent): boolean {
+    createEventHandler({ data }: PostMessageEvent): boolean | void {
         try {
             data && typeof data === 'string' && (data = JSON.parse(data))
         } catch {
@@ -71,18 +78,18 @@ class PostMessager implements Instance {
         else console.warn('没有注册type的执行方法')
     }
     // 移除message监听
-    removeEventHandler() {
+    removeEventHandler(): void {
         removeEvent(window, 'message', this.createEventHandler, false)
     }
     // 向上发送message
-    postMessageUp(actionName, content: any = {}, pageId): void {
+    postMessageUp(actionName: string, content: any = {}, pageId: string): void {
         let type = actionName
         if (this.type) {
             content.actionName = actionName
             type = this.type
         }
-        window !== parent.window &&
-            parent.window.postMessage(
+        (window as any) !== (parent as any).window &&
+            (parent as any).window.postMessage(
                 JSON.stringify({
                     type,
                     content,
@@ -93,14 +100,14 @@ class PostMessager implements Instance {
             )
     }
     // 向下发送message
-    postMessageDown(name: string, actionName: string, content: Content, pageId: string): void {
+    postMessageDown(name: string | null, actionName: string, content: Content, pageId: string): void {
         let type = actionName
         if (this.type) {
             content.actionName = actionName
             type = this.type
         }
         if (name) {
-            window.frames[name].postMessage(
+            (window as any).frames[name].postMessage(
                 JSON.stringify({
                     type,
                     content: {},
@@ -110,8 +117,8 @@ class PostMessager implements Instance {
                 '*'
             )
         } else {
-            for (let i = 0; i < window.frames.length; i++) {
-                window.frames[i].postMessage(
+            for (let i = 0; i < (window as any).frames.length; i++) {
+                (window as any).frames[i].postMessage(
                     JSON.stringify({
                         type,
                         content,
