b44e78cff5c9bc78e15aff8912c7f54bcfb6368c
saqqdy <saqqdy@qq.com>
2022-08-05 00:03:07 +0800

    update packages & add workflow

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -2,4 +2,4 @@ import { execSync } from 'child_process'
 
 const cmd = process.platform === 'win32' ? 'start' : 'open'
 
-execSync(`${cmd} https://npmmirror.com/sync/axios-ex`)
+execSync(`${cmd} https://npmmirror.com/sync/postmessager`)
